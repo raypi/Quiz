dc9d9fa50cec2f4ab45a5534f7f0afde8a43382b
Ray <rainer.musch@gmx.de>
2024-12-11 18:05:22 +0100

    When you click on the button you open the next question and reset all elements. Disable from the “next question” -button

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function init(){
 
 
 // aktuelle Frage und die dazugehörigen Antworten anzeigen
-function showQuestion(questionTxt){
+function showQuestion(){
     let question = questions[currentQuestion]; // ertellt Container "question" und holt aus dem container "questions" die erste stelle, (0), heraus
 
     document.getElementById('questionTxt').innerHTML = question['question'];
@@ -89,20 +89,41 @@ function showQuestion(questionTxt){
 
 // Auswahl der richtigen Antwort aus den Antwortmöglichkeiten
 function answer(selection){ // Inhalt der Variable selection wird vom Buttun per onclick übergeben
-    let question = questions[currentQuestion]; // Hilfsvariable question erhält Index von questions
-    console.log('Selected answer is ', selection); // gibt in Console den Betätigten Button aus zum testen
-    let selectedQuestionNumber = selection.slice(-1); // nimmt die letzte Stelle der Variable selection die vom Button übergeben wird
-    console.log('Current question is', question['rightAnswer']); // ausgabe Console der richtigen Antwort!
+    let question = questions[currentQuestion]; // Hilfsvariable question erhält Index von questions    console.log('Selected answer is ', selection); // gibt in Console den Betätigten Button aus zum testen
+    let selectedQuestionNumber = selection.slice(-1); // nimmt die letzte Stelle der Variable selection die vom Button übergeben wird    console.log('Current question is', question['rightAnswer']); // ausgabe Console der richtigen Antwort!
     
     
     let idOfRightAnswer = `answer${question['rightAnswer']}`; // ID der Richtigen Antwort ind die Hilfvariable geben
     
+
     if(selectedQuestionNumber == question['rightAnswer']) { // prüft ob gegebene Antwort der Richtigen Antwort entspricht
-        console.log('Richtige Antwort!'); // wenn die Antwort richtig ist erfolgt die Ausgabe, richtige Antwort
         document.getElementById(selection).parentNode.classList.add('bg-success'); // classList.add('bg-succes') fügt dem Element die CSS Class bg-sucess hinzu, dabei greift die Variable selection direkt auf die ID des DIV zu 
     } else {
-        console.log('falsche Antwort!') // bedingung nicht erfüllt - dann ausgabe Flasche Antwort
-        document.getElementById(selection).parentNode.classList.add('bg-danger');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        document.getElementById(selection).parentNode.classList.add('bg-danger'); // siehe Farbwechsel bi IF
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); // Richtige Antwort grün markieren mit der Hilfsvariable idOfRightAnswer (setzt) Ziffer und answer zusammen für ID
     }
+    document.getElementById('nextButton').disabled = false; // der "nächste Frage" Butten ist enabled bis eine Antwort gegeben wird und danch ist er disabled
+}
+
+
+// Funktion nächste Frage aufrufen
+function nextQuestion (){
+    currentQuestion++; // Variable erhöhen zum Beispiel von 0 auf 1
+    document.getElementById('nextButton').disabled = true; // der "nächste Frage" Butten wird auf disabled gesetzt
+    resetAnswerButtons(); // Aufruf der Funktion
+    showQuestion(); // ruft die Funktion showQuestion auf
+    // Anzeige der Frage von X Fragen aktualisieren
+}
+
+
+// Funktion zum zurücksetzen der farben der Antwort DIV s 
+function resetAnswerButtons(){
+    document.getElementById('answer1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer4').parentNode.classList.remove('bg-success');
 }
